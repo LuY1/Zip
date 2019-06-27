46f0dd4079038f88e96430a3eb7f1eb4554e82da
LuY1 <42060752+LuY1@users.noreply.github.com>
2019-06-27 17:05:30 +0800

    Update ZipFunction.h

--- Zip/ZipFunction.h
+++ Zip/ZipFunction.h
@@ -11,14 +11,16 @@
 #include <string>
 #include <vector>
 
+#pragma warning(disable : 4996) 
+
 /*******************************************************************************
 **  AREA2  : code function declare area
 *******************************************************************************/
 
-ZRESULT ExtractZipToDir(const char* unzipSrcPath, const char* unzipDestPath = NULL);
+ZRESULT ExtractZipToDir(const char* unzipSrcPath, const char* unzipDestPath = NULL, std::string unzipPassw = "");
 ZRESULT CompressDirToZip(const char* zipSrcPath, const char* zipDestPath = NULL);
 ZRESULT CompressDirToZip(std::vector<std::string> vzipSrcPath, const char* czipDestPath);
-ZRESULT ExtractZipToDir(LPCTSTR unzipSrcPath, LPCTSTR unzipDestPath = NULL);
+ZRESULT ExtractZipToDir(LPCTSTR unzipSrcPath, LPCTSTR unzipDestPath = NULL, std::string unzipPassw = "");
 ZRESULT CompressDirToZip(LPCTSTR zipSrcPath, LPCTSTR zipDestPath = NULL);
 
 
